27fbcea08d4d458ee4aff30859e733f0999e721e
Leonardo Augusto <leonerd12@gmail.com>
2014-09-29 16:22:32 -0400

    [ADD] Arquivos adicionados.
    Signed-off-by: Leonardo Augusto <leonerd12@gmail.com>

---
+++ module/Livraria/src/Livraria/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Livraria\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController {
+
+    public function indexAction() {
+        $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $repo = $em->getRepository('Livraria\Entity\Categoria')->findAll();
+        return new ViewModel(array('categorias' => $repo));
+    }
+}
---
+++ module/Livraria/src/Livraria/Entity/Categoria.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Livraria\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categorias")
+ */
+class Categoria {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    protected $nome;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function __toString() {
+        return $this->nome;
+    }
+    
+    public function toArray() {
+        return array('id' => $this->id, 'nome'=>  $this->nome);
+    }
+}
